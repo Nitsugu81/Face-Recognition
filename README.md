484f90696be6efddf5b4b5816c41370a9f539bab
Nitsugu81 <augustin.mons@insa-lyon.fr>
2023-12-22 13:38:37 +0100

    Readme + main commenté

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-https://medium.com/@ageitgey/machine-learning-is-fun-part-4-modern-face-recognition-with-deep-learning-c3cffc121d78
+# Face Recognition with OpenCV and DeepFace
+## Overview
+
+This Python script uses OpenCV for video capture and face detection, and DeepFace for face recognition. It continuously captures video frames from the webcam, checks for face matches with reference images, and displays the result on the video feed.
+
+
+## Improved version of the tutorial
 
 https://www.youtube.com/watch?v=pQvkoaevVMk
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import threading
 import os
-
 import cv2
 from deepface import DeepFace
 
+# Initialize the video capture object
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+# Initialize variables for face matching
 counter = 0
 face_match = False
 matched_image = None
@@ -19,11 +20,13 @@ images_folder = "Images"
 # Get the list of image files in the folder
 image_files = [f for f in os.listdir(images_folder) if f.endswith(('.jpg', '.jpeg', '.png'))]
 
+# Function to check if a face in the frame matches any reference image
 def check_face(frame):
     global face_match, matched_image
     try:
         for image_file in image_files:
             reference_img = cv2.imread(os.path.join(images_folder, image_file))
+            # Use DeepFace to verify if the face in the frame matches the reference image
             if DeepFace.verify(frame, reference_img.copy())['verified']:
                 with face_match_lock:  # Acquire the lock before modifying face_match and matched_image
                     face_match = True
@@ -38,35 +41,47 @@ def check_face(frame):
             face_match = False
             matched_image = None
 
-
+# Flag to check if the window has been created for the matched image
 cst = False
+
+# Main loop for video capture and face matching
 while True:
     ret, frame = cap.read()
 
     if ret:
+        # Check face every 30 frames to reduce processing frequency
         if counter % 30 == 0:
             try:
                 threading.Thread(target=check_face, args=(frame.copy(),)).start()
             except ValueError:
                 pass
         counter += 1
-        with face_match_lock:  # Acquire the lock before reading face_match and matched_image
+
+        # Acquire the lock before reading face_match and matched_image
+        with face_match_lock:
             if face_match:
+                # Display "MATCH!" on the video frame if a match is found
                 cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
                 if matched_image is not None:
+                    # Display the matched image in a separate window
                     cv2.imshow('Matched Image', matched_image)
                     cv2.moveWindow('Matched Image', 700, 50)
-                    cst = True         
+                    cst = True
             else:
+                # Display "NO MATCH!" on the video frame if no match is found
                 cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
                 if cst == True:
+                    # If the window was created before, close it when there is no match
                     print("ok")
-                    cv2.destroyWindow('Matched Image')  # Close the window when there is no match
+                    cv2.destroyWindow('Matched Image')
 
+        # Display the video frame
         cv2.imshow('video', frame)
 
+    # Check for the 'q' key to exit the loop
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
 
+# Release the video capture object and close all windows
 cv2.destroyAllWindows()

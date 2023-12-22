2b376144e47064f745e5a4f7a557d7fa1aad9446
Nitsugu81 <augustin.mons@insa-lyon.fr>
2023-12-22 13:35:42 +0100

    affichage image qui match

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 counter = 0
 face_match = False
+matched_image = None
+face_match_lock = threading.Lock()  # Add a lock for synchronization
 
 # Specify the path to the folder containing images
 images_folder = "Images"
@@ -18,18 +20,26 @@ images_folder = "Images"
 image_files = [f for f in os.listdir(images_folder) if f.endswith(('.jpg', '.jpeg', '.png'))]
 
 def check_face(frame):
-    global face_match
+    global face_match, matched_image
     try:
         for image_file in image_files:
             reference_img = cv2.imread(os.path.join(images_folder, image_file))
             if DeepFace.verify(frame, reference_img.copy())['verified']:
-                face_match = True
+                with face_match_lock:  # Acquire the lock before modifying face_match and matched_image
+                    face_match = True
+                    matched_image = reference_img.copy()
                 break  # Break the loop if a match is found
             else:
-                face_match = False
+                with face_match_lock:  # Acquire the lock before modifying face_match and matched_image
+                    face_match = False
+                    matched_image = None
     except ValueError:
-        face_match = False
+        with face_match_lock:  # Acquire the lock before modifying face_match and matched_image
+            face_match = False
+            matched_image = None
 
+
+cst = False
 while True:
     ret, frame = cap.read()
 
@@ -40,10 +50,18 @@ while True:
             except ValueError:
                 pass
         counter += 1
-        if face_match:
-            cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
-        else:
-            cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+        with face_match_lock:  # Acquire the lock before reading face_match and matched_image
+            if face_match:
+                cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+                if matched_image is not None:
+                    cv2.imshow('Matched Image', matched_image)
+                    cv2.moveWindow('Matched Image', 700, 50)
+                    cst = True         
+            else:
+                cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+                if cst == True:
+                    print("ok")
+                    cv2.destroyWindow('Matched Image')  # Close the window when there is no match
 
         cv2.imshow('video', frame)
 
